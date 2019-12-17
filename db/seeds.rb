aa15bdf5d786fa4cdaef12167d2ce0bc25b87693
tomoya.sakata <tomoyakg190@gmail.com>
2019-12-17 05:10:03 +0000

    add db-seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+(1..100).each do |n|
+Task.create(status: 'test' + n.to_s, content: 'test' + n.to_s)
+end
